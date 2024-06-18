ef7929b094e67fa8039e4bc2ec9a3826cc74cdd6
krupa-patel789 <59404757+krupa-patel789@users.noreply.github.com>
2024-06-18 16:05:24 -0400

    Initial commit of Terraform project

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create an App Service Plan
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier     = "Standard"
+    size     = "S1"
+  }
+}
+
+# Create an App Service
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+}
+
+# Optional: Output the App Service URL
+output "app_service_default_hostname" {
+  value = azurerm_app_service.example.default_site_hostname
+}
